5da7d1db7aab0ef9d595f40d1d2a8ac233758845
John Vandenberg <jayvdb@gmail.com>
2016-04-04 11:42:16 +0700

    Write binary with serialize None

--- src/anzsrc/src/anzsrc/scripts/genanzsrc.py
+++ src/anzsrc/src/anzsrc/scripts/genanzsrc.py
@@ -10,11 +10,11 @@ OUTPUT_DIR = "anzsrc_output"
 def genoutput(gengraph, fname, base):
     g = gengraph()
     setnamespaceprefixes(g)
-    f = open(OUTPUT_DIR + "/" + fname + '.rdf', 'w')
+    f = open(OUTPUT_DIR + "/" + fname + '.rdf', 'wb')
     out = g.serialize(None, format='xml')
     f.write(out)
     f.close()
-    f = open(OUTPUT_DIR + "/" + fname + '.n3', 'w')
+    f = open(OUTPUT_DIR + "/" + fname + '.n3', 'wb')
     out = g.serialize(None, format='n3')
     f.write(out)
     f.close()
